cf994e75e4d8be5df009bd61ac9871da535a9e2e
naxwell <maxwell.lander@gmail.com>
2019-10-10 15:44:14 -0400

    Add files via upload
    colour changing on gaze with revert back to default colour.

---
+++ Gaze Colour Revert/colorChangeOnRay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChangeOnRay : MonoBehaviour
+{
+    public lookingAt lookingAt; //reference our lookingAt script
+    public Renderer Rend; //Renderers are in charge of how objects look 
+    public Color startingColor; // the default colour of this object 
+    public Color lookedAt; // the colour we want to change it to when it is looking at
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rend = GetComponent<Renderer>(); //get the renderer attached to this gameObject
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(lookingAt.raycasted = this.gameObject) // this is the reference to the public variable we created in the other script. it is what we the player is currently looking at
+        {                                        // we want to check if what we are looking at is this gameObject
+            Rend.material.SetColor("_Color", lookedAt); // if it is, change it to our lookedAt colour
+        } else
+        {
+            Rend.material.SetColor("_Color", startingColor); // if not, make our colour the default
+        }
+    }
+}
---
+++ Gaze Colour Revert/lookingAt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookingAt : MonoBehaviour
+{
+
+    public RaycastHit CastHit;
+    public GameObject raycasted; // stores the object player is looking at in a public variable to be accessed by other scripts
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out CastHit, Mathf.Infinity))
+        {
+
+            raycasted = CastHit.collider.gameObject; //sets the public variable above to the object being hit by the raycast
+
+        }
+    }
+}
