c2a67df4f53ddf9ebf5cf4ffff49bab27a99ef5c
naxwell <maxwell.lander@gmail.com>
2019-10-17 00:03:06 -0400

    simple jump script

---
+++ jump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+
+  
+    public float thrust = 10f;
+    public OVRPlayerController controller;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        controller = GetComponent<OVRPlayerController>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            controller.JumpForce = thrust;
+            controller.Jump();
+        }
+            
+    }
+}
